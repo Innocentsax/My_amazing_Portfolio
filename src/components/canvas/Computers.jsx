dd95a39685b348b3ad9768b8c78c0b154a2af8ef
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-08-13 13:16:25 +0100

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,66 +1,3 @@
-// import { Suspense, useEffect, useState } from "react";
-// import { Canvas } from '@react-three/fiber';
-// import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-// import CanvasLoader from '../Loader';
-
-
-
-
-// const Computers = () => {
-//   const computer = useGLTF('./desktop_pc/scene.gltf')
-//   return (
-//     <mesh>
-//       <hemisphereLight intensity={2.15} groundColor="black" />
-//       <pointLight intensity={1} />
-//       <spotLight 
-//         position={[-20, 50, 10]}
-//         angle={0.12}
-//         penumbra={1}
-//         intensity={1}
-//         castShadow
-//         shadow-mapSize={1024}
-//       />
-//       <primitive object={computer.scene}
-//         scale={0.75}
-//         position= {[0, -3.25, -1.5]}
-//         rotation={[-0.01, -0.2, -0.1]}
-//       />
-//     </mesh>
-//   )
-// }
-
-
-// const ComputersCanvas = () => {
-//   return (
-//     <Suspense fallback={<CanvasLoader />}>
-//       <Canvas
-//         frameLoop="demand"
-//         shadows
-//         camera={{ position: [20, 3, 5], fov: 25 }}
-//         gl = {{ preserveDrawingBuffer: true }}
-//       >
-        
-//           <OrbitControls  
-//             enableZoom={false}
-//             maxPolarAngle={Math.PI / 2}
-//             minPolarAngle={Math.PI / 2}
-            
-//           />
-//           <Computers />
-        
-
-//         <Preload all />
-
-//       </Canvas>
-//     </Suspense>
-//   )
-
-
-// }
-
-// export default ComputersCanvas
-
-
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
@@ -84,7 +21,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.5 : 0.75}
         position={isMobile ? [0, -4.0, -2.2] : [0, -4.00, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
